73c451fb5b7fcbe627a3dca478a03cfc668d79f0
cur10sDEV <specsx@protonmail.com>
2022-03-18 23:08:13 +0530

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,109 @@
-// import './App.css';
+import "./App.css";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Button,
+  Toolbar,
+  Container,
+} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { PhotoCamera } from "@mui/icons-material";
 
 function App() {
-  return ();
+  const cardsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className="icon" />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className="hero-section">
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is a photo album and I'm trying to make this
+              sentence as long as I can to see how does it look like on screen.
+            </Typography>
+            <div className="button-container">
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className="card-container" maxWidth="md">
+          <Grid container spacing={4}>
+            {cardsArray.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className="card">
+                  <CardMedia
+                    className="card-media"
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className="card-content">
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Something here to give footer a purpose.
+        </Typography>
+      </footer>
+    </>
+  );
 }
 
 export default App;
